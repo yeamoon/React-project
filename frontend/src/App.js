75baa87205c4a6f6eccc1aa029f3c4e2b5c3b438
yeamon <yeamoonsultana@gmail.com>
2024-06-24 06:50:11 -0400

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+import Home from './containers/Home';
+
+
+
+
+
+
+
+
+
+const App = () => (
+ 
+        <Router>
+            
+                <Routes>
+                <Route path="/" element={<Home />} />
+                
+                 
+                </Routes>
+           
+        </Router>
+
+);
+
+export default App;
